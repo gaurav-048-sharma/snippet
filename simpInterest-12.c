2a4cf1782710656c2bd0a83c2ea19acba4388ce4
rohan <rohansharmas028@gmail.com>
2023-12-06 21:21:18 +0530

    fixed

--- simpInterest-12.c
+++ simpInterest-12.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main() {
-    int p, t, simpleInterest;
-    float r;
+    int p, t;
+    float r, simpleInterest;
     printf("Enter the principal amount: ");
     scanf("%d", &p);
     printf("Enter the time in years: ");
@@ -9,6 +9,6 @@ int main() {
     printf("Enter the rate of interest: ");
     scanf("%f", &r);
     simpleInterest = (p * t) * r / 100;
-    printf("\nThe Simple Interest is %d.", simpleInterest);
+    printf("\nThe Simple Interest is %f.", simpleInterest);
     return 0;
 }
