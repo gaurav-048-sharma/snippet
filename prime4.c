fc8379720e73fd134144229a48873f48507c0103
rohan <rohansharmas028@gmail.com>
2023-12-06 20:57:48 +0530

    fixed

--- prime4.c
+++ prime4.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    int i, j, prime, num;
+    int num, i;
 
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    for (i = 2; i <= num; i++) {
-        prime = 1;
-
-        for (j = 2; j <= i / 2; j++) {
-            if (i % j == 0) {
-                prime = 0;
+    if (num <= 1) {
+        printf("%d is not a prime number.\n", num);
+    } else {
+        for (i = 2; i <= num / 2; ++i) {
+            if (num % i == 0) {
+                printf("%d is not a prime number.\n", num);
                 break;
             }
         }
 
-        if (prime) {
-            printf("%d ", i);
+        if (i > num / 2) {
+            printf("%d is a prime number.\n", num);
         }
     }
 
