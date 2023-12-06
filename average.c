0e119a29b13ca6e2b885bea2ae26e467d5ef38f5
rohan <rohansharmas028@gmail.com>
2023-12-06 20:11:55 +0530

    added

--- average.c
+++ average.c
@@ -1,13 +1,9 @@
 #include<stdio.h>
 int main() {
-    int n, x, y, a,b, sum, average;
+    int a,b,c, sum, average;
     printf("enter the 3 dgit value: ");
-    scanf("%d", &n);
-    x = n/100;
-    y = n%100;
-    a = y/10;
-    b = y%10;
-    sum = x+a+b;
+    scanf("%d %d %d", &a, &b, &c);
+    sum = a+b+c;
     average = sum/3;
     printf("sum is : %d\n", sum);
     printf("average is : %d", average);
